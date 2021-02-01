046c6426006cccf211956f251fb5152159f69df5
Luis Moncada <luisdavidmoncad@gmail.com>
2021-02-01 18:17:48 -0400

    test and packache to production

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const { config } = require('./config')
+const helmet = require("helmet");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { config } = require("./config");
 
-const userRoute = require('./routes/user')
-const categoryRoute = require('./routes/category')
-const loginRoute = require('./routes/auth')
-const productRoute = require('./routes/product')
-const imageRoute = require('./routes/images')
-const useOrder = require('./routes/order');
-const dashboardRoute = require("./routes/dashboard")
+const userRoute = require("./routes/user");
+const categoryRoute = require("./routes/category");
+const loginRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const imageRoute = require("./routes/images");
+const useOrder = require("./routes/order");
+const dashboardRoute = require("./routes/dashboard");
 
-require('./database')
+require("./database");
 const accepCors = {
-    origin: config.frontendUrl
-}
-app.use(cors())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+  origin: config.frontendUrl,
+};
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
+);
 
-app.use( fileUpload({ useTempFiles: true }) );
+app.use(fileUpload({ useTempFiles: true }));
 
-
-
-app.use('/api/users', userRoute);
-app.use('/api/categories', categoryRoute);
-app.use('/api/products', productRoute);
-app.use('/api/', loginRoute);
-app.use('/api/images', imageRoute);
-app.use("/api/order", useOrder)
-app.use("/api/dashboard", dashboardRoute)
-
+app.use("/api/users", userRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/", loginRoute);
+app.use("/api/images", imageRoute);
+app.use("/api/order", useOrder);
+app.use("/api/dashboard", dashboardRoute);
 
 app.listen(8000, () => {
-    console.log('se ha conectado')
+  console.log("se ha conectado");
 });
